6ebebd92218b9d119974f0d376d35584880cc5ae
liyalin <1063357883@qq.com>
2019-10-10 10:59:11 +0800

    YunAppTheme+Font 添加字体大小接口

--- YunBaseApp/YunBaseApp/Classes/Theme/YunAppTheme+Font.h
+++ YunBaseApp/YunBaseApp/Classes/Theme/YunAppTheme+Font.h
@@ -7,15 +7,43 @@
 
 @interface YunAppTheme (Font)
 
++ (UIFont *)fontL_S;
+
++ (UIFont *)fontN_S;
+
++ (UIFont *)fontM_S;
+
++ (UIFont *)fontB_S;
+
 + (UIFont *)fontL_N;
 
 + (UIFont *)fontN_N;
 
++ (UIFont *)fontM_N;
+
 + (UIFont *)fontB_N;
+
++ (UIFont *)fontL_L;
+
++ (UIFont *)fontN_L;
+
++ (UIFont *)fontM_L;
+
++ (UIFont *)fontB_L;
+
++ (UIFont *)fontL_B;
+
++ (UIFont *)fontN_B;
+
++ (UIFont *)fontM_B;
+
++ (UIFont *)fontB_B;
 
 + (UIFont *)fontL:(CGFloat)size;
 
 + (UIFont *)fontN:(CGFloat)size;
+
++ (UIFont *)fontM:(CGFloat)size;
 
 + (UIFont *)fontB:(CGFloat)size;
 
--- YunBaseApp/YunBaseApp/Classes/Theme/YunAppTheme+Font.m
+++ YunBaseApp/YunBaseApp/Classes/Theme/YunAppTheme+Font.m
@@ -7,6 +7,22 @@
 
 @implementation YunAppTheme (Font)
 
++ (UIFont *)fontL_S {
+    return [UIFont fontWithName:YunAppTheme.instance.fontLName size:YunAppTheme.instance.sizeS];
+}
+
++ (UIFont *)fontN_S {
+    return [UIFont fontWithName:YunAppTheme.instance.fontNName size:YunAppTheme.instance.sizeS];
+}
+
++ (UIFont *)fontM_S {
+    return [UIFont fontWithName:YunAppTheme.instance.fontMName size:YunAppTheme.instance.sizeS];
+}
+
++ (UIFont *)fontB_S {
+    return [UIFont fontWithName:YunAppTheme.instance.fontBName size:YunAppTheme.instance.sizeS];
+}
+
 + (UIFont *)fontL_N {
     return [UIFont fontWithName:YunAppTheme.instance.fontLName size:YunAppTheme.instance.sizeN];
 }
@@ -15,9 +31,46 @@
     return [UIFont fontWithName:YunAppTheme.instance.fontNName size:YunAppTheme.instance.sizeN];
 }
 
++ (UIFont *)fontM_N {
+    return [UIFont fontWithName:YunAppTheme.instance.fontMName size:YunAppTheme.instance.sizeN];
+}
+
 + (UIFont *)fontB_N {
     return [UIFont fontWithName:YunAppTheme.instance.fontBName size:YunAppTheme.instance.sizeN];
 }
+
++ (UIFont *)fontL_L {
+    return [UIFont fontWithName:YunAppTheme.instance.fontLName size:YunAppTheme.instance.sizeL];
+}
+
++ (UIFont *)fontN_L {
+    return [UIFont fontWithName:YunAppTheme.instance.fontNName size:YunAppTheme.instance.sizeL];
+}
+
++ (UIFont *)fontM_L {
+    return [UIFont fontWithName:YunAppTheme.instance.fontMName size:YunAppTheme.instance.sizeL];
+}
+
++ (UIFont *)fontB_L {
+    return [UIFont fontWithName:YunAppTheme.instance.fontBName size:YunAppTheme.instance.sizeL];
+}
+
++ (UIFont *)fontL_B {
+    return [UIFont fontWithName:YunAppTheme.instance.fontLName size:YunAppTheme.instance.sizeB];
+}
+
++ (UIFont *)fontN_B {
+    return [UIFont fontWithName:YunAppTheme.instance.fontNName size:YunAppTheme.instance.sizeB];
+}
+
++ (UIFont *)fontM_B {
+    return [UIFont fontWithName:YunAppTheme.instance.fontMName size:YunAppTheme.instance.sizeB];
+}
+
++ (UIFont *)fontB_B {
+    return [UIFont fontWithName:YunAppTheme.instance.fontBName size:YunAppTheme.instance.sizeB];
+}
+
 
 + (UIFont *)fontL:(CGFloat)size {
     return [UIFont fontWithName:YunAppTheme.instance.fontLName size:[YunAppTheme.instance sSize:size]];
@@ -25,6 +78,10 @@
 
 + (UIFont *)fontN:(CGFloat)size {
     return [UIFont fontWithName:YunAppTheme.instance.fontNName size:[YunAppTheme.instance sSize:size]];
+}
+
++ (UIFont *)fontM:(CGFloat)size {
+    return [UIFont fontWithName:YunAppTheme.instance.fontMName size:[YunAppTheme.instance sSize:size]];
 }
 
 + (UIFont *)fontB:(CGFloat)size {
